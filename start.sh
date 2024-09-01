4cffae4b16a3835df42a45a87591090af245e227
rm <xzxn099@gmail.com>
2024-09-01 16:46:48 -0300

    -

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-screen -S minecraft -d -m java -Xmx1024M -Xms1024M -jar server.jar nogui --server-properties ../$PROPERTIES_FILE
+java -Xmx1024M -Xms1024M -jar server.jar nogui --server-properties ../$PROPERTIES_FILE
 
 echo "Minecraft server installed and started in a screen session named 'minecraft'."
 echo "To access the console, use 'screen -r minecraft'."
