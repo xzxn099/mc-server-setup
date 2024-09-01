2f242a286d97a7081683c581e419d93058af9273
rm <xzxn099@gmail.com>
2024-09-01 17:57:27 -0300

    -

--- setup.sh
+++ setup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-SERVER_DIR="./dist"
-
-PROPERTIES_FILE="./server/server.properties"
-
 sudo apt install -y openjdk-17-jre-headless screen
 
-mkdir -p $SERVER_DIR
-cd $SERVER_DIR
+SERVER_DIR="/mc-server"
 
-wget https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar
+sudo mkdir $SERVER_DIR
+
+cp ./server.properties $SERVER_DIR
+cp ./start.sh $SERVER_DIR 
+
+cd $SERVER_DIR
 
 echo "eula=true" > eula.txt
 
+wget https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar
 
+if ! grep -q "^alias mc-start=" ~/.bashrc; then
+    echo "alias mc-start=\"$SERVER_DIR/start.sh\"" >> ~/.bashrc
+    echo "Alias 'mc-start' added to your .bashrc. Open a new terminal or run 'source ~/.bashrc' to use it."
+else
+    echo "Alias 'mc-start' already exists. No changes made."
+fi
--- start.sh
+++ start.sh
@@ -1,5 +1,1 @@
-java -Xmx1024M -Xms1024M -jar ./dist/server.jar nogui --server-properties ../$PROPERTIES_FILE
-
-echo "Minecraft server installed and started in a screen session named 'minecraft'."
-echo "To access the console, use 'screen -r minecraft'."
-echo "You can configure the server by editing the 'server.properties' file."
+java -Xmx1024M -Xms1024M -jar ./dist/server.jar nogui --server-properties ./server.properties
